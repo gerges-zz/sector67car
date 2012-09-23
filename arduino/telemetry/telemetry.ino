55afebb67b5fce52d882caf6cb235ed80a8d5a4f
Issac Gerges <gergesim@gmail.com>
2012-09-23 04:04:29 -0500

    Added HashSet impl

--- arduino/telemetry/telemetry.ino
+++ arduino/telemetry/telemetry.ino
@@ -2,6 +2,7 @@
 #include <Ethernet.h>
 #include <EthernetBonjour.h>
 #include <EthernetUdp.h>
+#include "HashSet.h"
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] = { 169, 254, 86, 154 };
@@ -9,13 +10,17 @@ byte ip[] = { 169, 254, 86, 154 };
 EthernetUDP udp;
 
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+//hashmap
+const byte HASH_SIZE = 5;
+HashType<char*,char*> hashRawArray[HASH_SIZE];
+HashSet<char*,char*> hashSet = HashSet<char*,char*>( hashRawArray , HASH_SIZE );
+
 char  reply[] = "acknowledged";   
 
 void setup() {
   Ethernet.begin(mac, ip);
   udp.begin(8888);
   Serial.begin(9600);
-  
   EthernetBonjour.begin("telemetry-arduino");
   EthernetBonjour.addServiceRecord("UDP._telemetry", 8888, MDNSServiceUDP);
 }
@@ -28,12 +33,9 @@ void loop() {
     // read the packet into packetBufffer
     udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
     if(strcmp(packetBuffer,"req") == 0) {
-      //request found
+      udp.beginPacket(udp.remoteIP(), udp.remotePort());
+      udp.write(reply);
+      udp.endPacket();
     }
-    
-    // send a reply, to the IP address and port that sent us the packet we received
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write(reply);
-    udp.endPacket();
   }
 }
